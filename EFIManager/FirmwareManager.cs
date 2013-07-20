67eaa6d28b6db90a93012f759b746013fd14c2fe
Scott <scott.goodhew0@gmail.com>
2013-07-20 16:49:27 +1200

    initial commit

---
+++ EFIManager/FirmwareManager.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFIManager
+{
+	public class FirmwareManager
+	{
+
+		#region DLLImports
+
+		[DllImport("kernel32.dll", EntryPoint = "GetFirmwareEnvironmentVariableW", SetLastError = true)]
+		public static extern uint GetFirmwareEnvironmentVariableW([MarshalAs(UnmanagedType.LPWStr)] string lpName, [MarshalAs(UnmanagedType.LPWStr)] string lpGuid, IntPtr pBuffer, uint nSize);
+
+		#endregion
+
+
+		private const string EFI_GLOBAL_VARIABLE = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
+
+		private Boolean debug = true;
+		public Boolean Debug { get { return debug; } set { debug = value; } }
+
+		public FirmwareManager()
+		{
+			if (!BitConverter.IsLittleEndian)
+			{
+				Console.WriteLine("Your system seems to not be Little Endian. Unsure what will happen here, good luck!");
+			}
+
+		}
+
+
+		public UInt16[] GetBootOrder()
+		{
+			byte[] sb = GetEfiVar("BootOrder", EFI_GLOBAL_VARIABLE);
+			UInt16[] ret = new UInt16[sb.Length / 2];
+
+			for (int i = 0; i < ret.Length; i++)
+			{
+				ret[i] = BitConverter.ToUInt16(sb, i * 2);
+			}
+			return ret;
+		}
+
+		public LoadOption GetBootInfo(UInt16 bootNumber)
+		{
+			byte[] buf = GetEfiVar("Boot" + bootNumber.ToString("X4"), EFI_GLOBAL_VARIABLE);
+			return new LoadOption(buf);
+		}
+
+
+		public byte[] GetEfiVar(string lpName, string lpGuid)
+		{
+			D("Reading {0}:{1}", lpName, lpGuid);
+			byte[] buf = new byte[1024];
+			IntPtr bufPtr = Marshal.AllocHGlobal(buf.Length);
+			Marshal.Copy(buf, 0, bufPtr, buf.Length);
+
+			uint size = GetFirmwareEnvironmentVariableW(lpName, lpGuid, bufPtr, (uint)buf.Length);
+
+			Marshal.Copy(bufPtr, buf, 0, (int)size);
+
+			Marshal.FreeHGlobal(bufPtr);
+
+			if (Marshal.GetLastWin32Error() != 0)
+			{
+				throw new Win32Exception(Marshal.GetLastWin32Error());
+			}
+			Array.Resize(ref buf, (int)size);
+
+			D("Size: " + size);
+			D("Ret: [{0}]", String.Join(",", buf));
+
+			return buf;
+		}
+
+
+		private void D(String msg, params object[] args)
+		{
+			if (debug)
+			{
+				Console.WriteLine("D:" + msg, args);
+			}
+		}
+
+
+
+		public void UpgradePrivilage()
+		{
+			IntPtr tokenHandle = IntPtr.Zero;
+
+			try
+			{
+				// get process token
+				if (!OpenProcessToken(Process.GetCurrentProcess().Handle,
+					TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES,
+					out tokenHandle))
+				{
+					throw new Win32Exception(Marshal.GetLastWin32Error(),
+						"Failed to open process token handle");
+				}
+
+				// lookup the shutdown privilege
+				TOKEN_PRIVILEGES tokenPrivs = new TOKEN_PRIVILEGES();
+				tokenPrivs.PrivilegeCount = 1;
+				tokenPrivs.Privileges = new LUID_AND_ATTRIBUTES[1];
+				tokenPrivs.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+				if (!LookupPrivilegeValue(null,
+					SE_SYSTEM_ENVIRONMENT_NAME,
+					out tokenPrivs.Privileges[0].Luid))
+				{
+					throw new Win32Exception(Marshal.GetLastWin32Error(),
+						"Failed to open lookup shutdown privilege");
+				}
+
+
+				// add the shutdown privilege to the process token
+				if (!AdjustTokenPrivileges(tokenHandle,
+					false,
+					ref tokenPrivs,
+					0,
+					IntPtr.Zero,
+					IntPtr.Zero))
+				{
+					throw new Win32Exception(Marshal.GetLastWin32Error(),
+						"Failed to adjust process token privileges");
+				}
+
+
+				Console.WriteLine("Privilages should be OK!");
+
+			}
+			finally
+			{
+				// close the process token
+				if (tokenHandle != IntPtr.Zero)
+				{
+					CloseHandle(tokenHandle);
+				}
+			}
+		}
+
+
+
+
+		#region Stuff for Privilage fixing
+		[StructLayout(LayoutKind.Sequential)]
+		private struct LUID
+		{
+			public uint LowPart;
+			public int HighPart;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct LUID_AND_ATTRIBUTES
+		{
+			public LUID Luid;
+			public UInt32 Attributes;
+		}
+
+		private struct TOKEN_PRIVILEGES
+		{
+			public UInt32 PrivilegeCount;
+			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+			public LUID_AND_ATTRIBUTES[] Privileges;
+		}
+
+
+		private const UInt32 TOKEN_QUERY = 0x0008;
+		private const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+		private const UInt32 SE_PRIVILEGE_ENABLED = 0x00000002;
+		// private const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+		private const string SE_SYSTEM_ENVIRONMENT_NAME = "SeSystemEnvironmentPrivilege";
+
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool OpenProcessToken(IntPtr ProcessHandle,
+			UInt32 DesiredAccess,
+			out IntPtr TokenHandle);
+
+		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool LookupPrivilegeValue(string lpSystemName,
+			string lpName,
+			out LUID lpLuid);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool CloseHandle(IntPtr hObject);
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+			[MarshalAs(UnmanagedType.Bool)]bool DisableAllPrivileges,
+			ref TOKEN_PRIVILEGES NewState,
+			UInt32 Zero,
+			IntPtr Null1,
+			IntPtr Null2);
+
+		[DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern bool LookupPrivilegeValue([MarshalAs(UnmanagedType.LPTStr)] string lpSystemName, [MarshalAs(UnmanagedType.LPTStr)] string lpName, out Int64 lpLuid);
+
+
+		#endregion
+
+
+	}
+}
---
+++ EFIManager/LoadOption.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFIManager
+{
+	public class LoadOption
+	{
+
+		public const uint LOAD_OPTION_ACTIVE = 0x00000001;
+		public const uint LOAD_OPTION_FORCE_RECONNECT = 0x00000002;
+		public const uint LOAD_OPTION_HIDDEN = 0x00000008;
+		public const uint LOAD_OPTION_CATEGORY = 0x00001F00;
+		public const uint LOAD_OPTION_CATEGORY_BOOT = 0x00000000;
+		public const uint LOAD_OPTION_CATEGORY_APP = 0x00000100;
+
+
+
+		private byte[] buf;
+
+		//UINT32 Attributes;
+		//UINT16 FilePathListLength;
+		//CHAR16 Description[];Unified Extensible Firmware Interface Specification
+		//EFI_DEVICE_PATH_PROTOCOL FilePathList[];
+		//UINT8 OptionalData[];
+
+
+		private UInt32? attributes = null;
+		private UInt16? filePathListLength = null;
+		private String description = null;
+		private DevicePath[] filePathList = null;
+		private byte[] optionalData;
+
+
+		public LoadOption(byte[] buf)
+		{
+			this.buf = buf;
+		}
+
+		public ushort FilePathListLength
+		{
+			get
+			{
+				return filePathListLength ?? (ushort)(filePathListLength = BitConverter.ToUInt16(buf, 4));
+			}
+		}
+
+
+
+		public String Description
+		{
+			get
+			{
+
+				if (description == null)
+				{
+					StringBuilder sb = new StringBuilder();
+					for (int i = 6; i < buf.Length; i += 2)
+					{
+						char c = (char)BitConverter.ToUInt32(buf, i);
+						if (c == 0)
+						{
+							break;
+						}
+						sb.Append(c);
+					}
+					description = sb.ToString();
+				}
+				return description;
+			}
+			set { }
+		}
+
+		public DevicePath[] FilePathLists
+		{
+			get
+			{
+				filePathList = new DevicePath[0];
+				int offset = 6 + Description.Length * 2 + 2;
+				for (int i = 0; i < FilePathListLength; )
+				{
+					DevicePath dp = DevicePath.GetDevice(buf, offset + i);
+					i += dp.Length;
+					Array.Resize(ref filePathList, filePathList.Length + 1);
+					filePathList[filePathList.Length - 1] = dp;
+				}
+
+				return filePathList;
+			}
+		}
+
+
+
+	}
+
+
+	public class DevicePath
+	{
+		protected byte[] buf;
+		protected int offset;
+
+		protected DevicePath(byte[] buf, int offset)
+		{
+			this.buf = buf;
+			this.offset = offset;
+		}
+
+		public DevicePath(DevicePath dp) : this(dp.buf, dp.offset) { }
+
+		public static DevicePath GetDevice(byte[] buf, int offset)
+		{
+			DevicePath device = new DevicePath(buf, offset);
+
+			DevicePathAttribute dpa = new DevicePathAttribute()
+			{
+				Type = device.Type,
+				SubType = device.SubType
+			};
+
+			if (DevicePathTypes.ContainsKey(dpa))
+			{
+				ConstructorInfo c = DevicePathTypes[dpa].GetConstructor(new Type[] { typeof(DevicePath) });
+
+				device = (c.Invoke(new object[] { device }) as DevicePath) ?? device;
+			}
+
+			return device;
+		}
+
+		public override string ToString()
+		{
+			return String.Format("Base DevicePath: {{type: 0x{0}, sub-type: 0x{1}, length: 0x{2}}}", Type.ToString("X2"), SubType.ToString("X2"), Length.ToString("X4"));
+		}
+
+
+
+
+		public byte Type
+		{
+			get
+			{
+				return buf[offset];
+			}
+		}
+
+		public byte SubType
+		{
+			get
+			{
+				return buf[offset + 1];
+			}
+		}
+
+		public ushort Length
+		{
+			get
+			{
+				return BitConverter.ToUInt16(buf, offset + 2);
+			}
+		}
+
+		private static Dictionary<DevicePathAttribute, Type> devicePathTypes = null;
+		private static Dictionary<DevicePathAttribute, Type> DevicePathTypes
+		{
+			get
+			{
+				if (devicePathTypes == null)
+				{
+					devicePathTypes = new Dictionary<DevicePathAttribute, Type>();
+					Assembly assembly = Assembly.GetExecutingAssembly();
+
+					foreach (Type type in assembly.GetTypes())
+					{
+						object[] attribs;
+						if ((attribs = type.GetCustomAttributes(typeof(DevicePathAttribute), true)).Length > 0)
+						{
+							foreach (object attrib in attribs)
+							{
+								DevicePathAttribute dpa = (DevicePathAttribute)attrib;
+								devicePathTypes.Add(dpa, type);
+							}
+						}
+					}
+
+
+				}
+
+
+				return devicePathTypes;
+			}
+		}
+
+	}
+
+	[DevicePath(Type = 0x04, SubType = 0x04)]
+	public class FilePathMediaDevicePath : DevicePath
+	{
+
+		public FilePathMediaDevicePath(DevicePath dp) : base(dp) { }
+
+		private string pathName = null;
+		public String PathName
+		{
+			get
+			{
+				return pathName ?? (pathName = Encoding.Unicode.GetString(buf, offset + 4, Length - 6));
+			}
+		}
+
+		public override string ToString()
+		{
+			return String.Format("FilePathMedia DevicePath: {{type: 0x{0}, sub-type: 0x{1}, PathName:{2}}}", Type.ToString("X2"), SubType.ToString("X2"), PathName);
+		}
+
+
+	}
+
+	[DevicePath(Type = 0x04, SubType = 0x01)]
+	public class HardDriveMediaDevicePath : DevicePath
+	{
+		public HardDriveMediaDevicePath(DevicePath dp) : base(dp) { }
+
+		public uint PartitionNumber
+		{
+			get
+			{
+				return BitConverter.ToUInt32(buf, offset + 4);
+			}
+		}
+
+		public ulong PartitionStart
+		{
+			get
+			{
+				return BitConverter.ToUInt64(buf, offset + 8);
+			}
+		}
+
+		public ulong PartitionSize
+		{
+			get
+			{
+				return BitConverter.ToUInt64(buf, offset + 16);
+			}
+		}
+
+		public byte[] PartitionSignature
+		{
+			get
+			{
+				byte[] ret = new byte[16];
+				Array.Copy(buf, offset + 24, ret, 0, 16);
+				return ret;
+			}
+		}
+
+		public byte PartitionFormat
+		{
+			get
+			{
+				return buf[offset + 40];
+			}
+		}
+		public byte SignatureType
+		{
+			get
+			{
+				return buf[offset + 41];
+			}
+		}
+
+
+		public override string ToString()
+		{
+			return String.Format("HardDriveMedia DevicePath: {{type: 0x{0}, sub-type: 0x{1}, PartitionNumber: {2}, PartitionStart: {3}, PartitionSize: {4}, PartitionFormat: 0x{5}, SignatureType: 0x{6} }}",
+				Type.ToString("X2"), SubType.ToString("X2"), PartitionNumber, PartitionStart, PartitionSize, PartitionFormat.ToString("X2"), SignatureType.ToString("X2"));
+		}
+
+
+
+	}
+
+	[DevicePath(Type = 0x7F, SubType = 0x01)]
+	[DevicePath(Type = 0x7F, SubType = 0xFF)]
+	public class EndOfHardwareDevicePath : DevicePath
+	{
+		public EndOfHardwareDevicePath(DevicePath dp) : base(dp) { }
+
+		public override string ToString()
+		{
+			return String.Format("EndOfHardware DevicePath: {{{0}, SubType: {1}}}", SubType==0x01?"End This Instance of a Device Path ": "End Entire Device Path", SubType.ToString("X2"));
+		}
+	}
+
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple=true)]
+	public class DevicePathAttribute : Attribute
+	{
+		public int Type { get; set; }
+		public int SubType { get; set; }
+
+		public override bool Equals(object o)
+		{
+			if (o == null)
+			{
+				return false;
+			}
+			if (o is DevicePathAttribute)
+			{
+				DevicePathAttribute dpa = o as DevicePathAttribute;
+				return dpa.Type == Type && dpa.SubType == SubType;
+			}
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return Type.GetHashCode() ^ SubType.GetHashCode();
+		}
+
+
+
+	}
+}
